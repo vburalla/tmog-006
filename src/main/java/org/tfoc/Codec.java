0feaf7c086f590f6e8aa484a7647d92ac9ae5e8e
VBB\User <victorburalla@gmail.com>
2024-02-26 23:34:35 +0100

    fix: first commit

--- src/main/java/org/tfoc/Codec.java
+++ src/main/java/org/tfoc/Codec.java
@@ -1,5 +1,8 @@
 package org.tfoc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * Your Codec object will be instantiated and called as such:
  * Codec serializer = new Codec();
@@ -13,15 +16,46 @@ public class Codec {
      */
     public String serialize(TreeNode root) {
 
-        return null;
+        StringBuilder sb = new StringBuilder("[");
+        if(root != null) {
+            sb.append(root.val);
+            LinkedList<TreeNode> nextNodes = new LinkedList<>();
+            if(root.left != null)
+                nextNodes.add(root.left);
+            if(root.right!=null)
+                nextNodes.add(root.right);
+            {
+                LinkedList<TreeNode> nextLevelNodes = new LinkedList<>();
+                while(!nextNodes.isEmpty()) {
+                    TreeNode node = nextNodes.pop();
+                    if(node!=null) {
+                        sb.append(node.val).append(",");
+                        nextLevelNodes.add(node.left);
+                        nextLevelNodes.add(node.right);
+                    } else {
+                        sb.append("null,");
+                    }
+                }
+                if(!nextLevelNodes.isEmpty())
+                    nextNodes.addAll(nextLevelNodes);
+                nextLevelNodes.clear();
+            }
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     /**
      * Decodes your encoded data to tree.
      */
     public TreeNode deserialize(String data) {
-
-        return null;
+        String[] values = data.replace("[","").replace("]","").split(",");
+        TreeNode root = null;
+        int i = 0;
+        if(values.length > 0) {
+            root = new TreeNode(Integer.valueOf(values[i]));
+        }
+        return root;
     }
 
 }
